ce95d726238a12fb7e7e5a11e68987c675eda0d7
Arnoldo Mora <arnoldomora@Arnoldos-MacBook-Air.local>
2016-09-27 11:58:39 -0600

    First commit

---
+++ flora-test-humidity-sensor-Beta1.0.ino
@@ -0,0 +1,35 @@
+int sensorPin = A11; //sensor de humedad
+int umbral_de_humedad = 900; //alertar si la lectura es mayor a este numero
+int led = 7;
+int First = 0;
+int Hum = 0;
+ 
+void setup() {
+ 
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW); //apagar el led para empezar
+ 
+}
+ 
+void loop() {
+ 
+  int sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+ 
+   if (First == 0){
+     delay(10000);
+     Hum = analogRead(sensorPin);
+     First = 1;
+     Serial.println("Listo!!!");
+   }
+   
+   if (sensorValue <= (Hum - 10)){
+     Serial.println("Alerta!");
+     digitalWrite(led, HIGH);
+     delay(1000);
+     digitalWrite(led, LOW);
+     delay(1000);
+   } 
+ 
+}
